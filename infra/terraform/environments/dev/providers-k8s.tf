e35ff63718de1cf7dcbd611e94b990f8e4bcfe6a
harrybrwnie <huanphan2497@gmail.com>
2026-06-04 14:30:32 +0000

    Fix EKS storage and Postgres data directory

--- infra/terraform/environments/dev/providers-k8s.tf
+++ infra/terraform/environments/dev/providers-k8s.tf
@@ -1,13 +1,9 @@
 data "aws_eks_cluster" "dev" {
   name = module.eks.cluster_name
-
-  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "dev" {
   name = module.eks.cluster_name
-
-  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
